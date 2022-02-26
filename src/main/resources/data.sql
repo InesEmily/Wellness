ec5dde88b5ed74b57c9fc1b1e284a3649eada8c3
ines <r0306129@student.thomasmore.be>
2022-02-26 16:12:36 +0100

    10 klanten toegevoegd aan de database

---
+++ src/main/resources/data.sql
@@ -0,0 +1,20 @@
+INSERT INTO "PUBLIC"."CLIENT" VALUES
+    (1, 'Frans stormstraat 20, 2812 Muizen', '1992-03-11', 'inesemilymertens@gmail.com', 'Mertens', 'Ines', 0478435049);
+INSERT INTO "PUBLIC"."CLIENT" VALUES
+    (2, 'Brusselsestraat 16,3000 Leuven', '2002-04-11', 'seppeTeugels@gmail.com', 'Teugels', 'Seppe', 0478435059);
+INSERT INTO "PUBLIC"."CLIENT" VALUES
+    (3, 'Zoetewei 15 , 2800 Mechelen', '1994-08-11', 'DriesDubois@gmail.com', 'Dubois', 'Dries', 0478435059);
+INSERT INTO "PUBLIC"."CLIENT" VALUES
+    (4, 'Leuvensesteenweg 28, 2812 Muizen', '1998-05-11', 'Chloe_claessen@gmail.com', 'Claessen', 'Chloë', 0488435049);
+INSERT INTO "PUBLIC"."CLIENT" VALUES
+    (5, 'Diestsestraat 15,3000 Leuven', '2000-09-11', 'Jennifer_ida@gmail.com', 'Ida', 'Jennifer', 0478455049);
+INSERT INTO "PUBLIC"."CLIENT" VALUES
+    (6, 'Diestsestraat 15,3000 Leuven', '2000-10-11', 'Wouter_ida@gmail.com', 'Ida', 'Wouter', 0478425049);
+INSERT INTO "PUBLIC"."CLIENT" VALUES
+    (7, 'Frans stormstraat 20, 2812 Muizen', '2000-04-25', 'Mertens_niels@gmail.com', 'Mertens', 'Niels', 0478415049);
+INSERT INTO "PUBLIC"."CLIENT" VALUES
+    (8, 'cardenberch 12 , 3000 Leuven', '2011-09-23', 'Verheyden_Dries@gmail.com', 'Verheyden', 'Dylan', 0478465049);
+INSERT INTO "PUBLIC"."CLIENT" VALUES
+    (9, 'fonteinstraat 28,3000 Leuven', '1998-08-01', 'Gillis_Maartena@gmail.com', 'Gillis', 'Maarten', 0478445049);
+INSERT INTO "PUBLIC"."CLIENT" VALUES
+    (10, 'vismarkt 15,2800 Mechelen', '1996-11-13', 'Verlinden_Mieke@gmail.com', 'Verlinden', 'Mieke', 0478495049);
