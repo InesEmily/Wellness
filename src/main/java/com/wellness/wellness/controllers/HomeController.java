492930cf6b4f224f8ebaf5ad9fde3eae71cedbc0
ines <r0306129@student.thomasmore.be>
2022-03-10 11:52:41 +0100

    Javascript bestand aangemaakt en gelinkt met HTML
    To Do : button meer info aanpassen
    To Do :  discover linken

--- src/main/java/com/wellness/wellness/controllers/HomeController.java
+++ src/main/java/com/wellness/wellness/controllers/HomeController.java
@@ -39,4 +39,5 @@ public class HomeController {
     public String viproom(){
         return "viproom";
     }
+
 }
--- src/test/java/com/wellness/wellness/WellnessApplicationTests.java
+++ src/test/java/com/wellness/wellness/WellnessApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class BowlingApplicationTests {
+class WellnessApplicationTests {
 
     @Test
     void contextLoads() {
